576b2e00ca721c60a5604cc1547f5f2b68c473c9
Diyac004 <diyachaudhari004@gmail.com>
2024-07-08 14:35:08 -0700

    minor fixes

--- utils/database.js
+++ utils/database.js
@@ -12,7 +12,7 @@ export const connectToDB = async () => {
 
   try {
     await mongoose.connect(process.env.MONGODB_URI, {
-      dbName: "share_prompt",
+      dbName: "promptme",
       useNewUrlParser: true,
       useUnifiedTopology: true,
     })
