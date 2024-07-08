5e97016a1c1cd5493a81022172c3f350e232118d
Diyac004 <diyachaudhari004@gmail.com>
2024-07-08 02:37:03 -0700

    ..

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ const Home = () => (
     <h1 className='head_text text-center'>
       Discover & Share
       <br className='max-md:hidden' />
-      <span className='red_gradient text-center'> AI-Powered Prompts</span>
+      <span className='red_gradient text-center'> Real-Life Instances</span>
     </h1>
     <p className='desc text-center'>
     PromptME is an open-source platform for modern world to discover, create and share their Real-Life experiences and help others in the similar scenearios to make the right decision
