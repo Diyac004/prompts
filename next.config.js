47bae3a414a666a2f0ab578765bc9dcc80ba6aef
Diyac004 <148058872+Diyac004@users.noreply.github.com>
2024-07-10 04:33:49 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
     serverComponentsExternalPackages: ["mongoose"],
   },
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com'],['https://promptme-git-main-diyac004s-projects.vercel.app']
   },
   webpack(config) {
     config.experiments = {
